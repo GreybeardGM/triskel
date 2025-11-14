693897229889bd0b5dd1ef8cfd2892e948c309f5
GreybeardGM <webmaster@sector-001.de>
2025-11-14 10:38:46 +0100

    Fix player character sheet rendering with Handlebars mixin

--- module/actor/sheets/player-character-sheet.js
+++ module/actor/sheets/player-character-sheet.js
@@ -1,8 +1,19 @@
-export class PlayerCharacterSheet extends ActorSheet {
-  static get defaultOptions() {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ["triskel", "sheet", "actor", "character"],
-      template: "systems/triskel/templates/actor/player-character-sheet.hbs"
-    });
+const { ActorSheetV2 } = foundry.applications.sheets;
+const { HandlebarsApplicationMixin } = foundry.applications.api;
+
+export class PlayerCharacterSheet extends HandlebarsApplicationMixin(ActorSheetV2) {
+  static DEFAULT_OPTIONS = foundry.utils.mergeObject(super.DEFAULT_OPTIONS, {
+    classes: ["triskel", "sheet", "actor", "character"],
+    template: "systems/triskel/templates/actor/player-character-sheet.hbs",
+    window: {
+      title: "Triskel | Player Character"
+    }
+  });
+
+  async _prepareContext(options) {
+    const context = await super._prepareContext(options);
+    context.system ??= this.actor.system;
+    context.reserves = context.system.reserves ?? {};
+    return context;
   }
 }
