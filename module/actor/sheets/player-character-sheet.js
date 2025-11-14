f61c87dfa8992239883511afd26922d81c7be683
GreybeardGM <webmaster@sector-001.de>
2025-11-14 10:22:17 +0100

    Update and rename triskel-character-sheet.js to player-character-sheet.js

--- module/actor/sheets/player-character-sheet.js
+++ module/actor/sheets/player-character-sheet.js
@@ -1,8 +1,8 @@
-export class TriskelCharacterSheet extends ActorSheet {
+export class PlayerCharacterSheet extends ActorSheet {
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ["triskel", "sheet", "actor", "character"],
-      template: "systems/triskel/templates/actor/character-sheet.hbs"
+      template: "systems/triskel/templates/actor/player-character-sheet.hbs"
     });
   }
 }
