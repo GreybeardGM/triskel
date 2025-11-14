1433aa999a8193f96aff67f2e4bed6b9ecbc90a7
GreybeardGM <webmaster@sector-001.de>
2025-11-14 22:39:19 +0100

    Update player-character.js

--- module/actor/player-character.js
+++ module/actor/player-character.js
@@ -1,7 +1,5 @@
 import { TriskelActor } from "./triskel-actor.js";
 
-const RESERVE_TYPES = ["power", "grace", "will"];
-
 export class PlayerCharacter extends TriskelActor {
   /** @override */
   prepareBaseData() {
