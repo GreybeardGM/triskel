8afd9f55b00a1acd910ee814ce6812bfdf28033c
GreybeardGM <webmaster@sector-001.de>
2025-11-14 01:52:00 +0100

    Update triskel-actor.js

--- module/actor/triskel-actor.js
+++ module/actor/triskel-actor.js
@@ -8,11 +8,7 @@ export class TriskelActor extends ActorSheet {
     const data = await super.getData(options);
     const r = data.system.reserves;
 
-    // 1..12 als Array
-    const steps = Array.from({ length: 12 }, (_, i) => i + 1);
-
     data.triskel = {
-      steps,
       powerValue: Number(r.power?.value ?? 0),
       graceValue: Number(r.grace?.value ?? 0),
       willValue: Number(r.will?.value ?? 0)
