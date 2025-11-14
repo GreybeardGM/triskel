79d956c4d2f92baebcf7a01397bc40a39f951e86
GreybeardGM <webmaster@sector-001.de>
2025-11-14 10:28:26 +0100

    Add player character actor and sheet using Foundry App V2

--- module/actor/sheets/player-character-sheet.js
+++ module/actor/sheets/player-character-sheet.js
@@ -1,8 +1,20 @@
-export class PlayerCharacterSheet extends ActorSheet {
+const { ActorSheetV2 } = foundry.applications.sheets;
+
+export class PlayerCharacterSheet extends ActorSheetV2 {
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ["triskel", "sheet", "actor", "character"],
-      template: "systems/triskel/templates/actor/player-character-sheet.hbs"
+      template: "systems/triskel/templates/actor/player-character-sheet.hbs",
+      window: {
+        title: "Triskel | Player Character"
+      }
     });
+  }
+
+  async getData(options) {
+    const context = await super.getData(options);
+    context.system ??= this.actor.system;
+    context.reserves = context.system.reserves ?? {};
+    return context;
   }
 }
