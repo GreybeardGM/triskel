3db7b4a0985aa4e7139df3b6fab7cdd4919a92bd
GreybeardGM <webmaster@sector-001.de>
2025-11-15 23:24:17 +0100

    Rework reserves layout and derived minimum

--- module/actor/player-character.js
+++ module/actor/player-character.js
@@ -9,5 +9,15 @@ export class PlayerCharacter extends TriskelActor {
   /** @override */
   prepareDerivedData() {
     super.prepareDerivedData();
+
+    const reserves = this.system?.reserves ?? {};
+    for (const reserve of Object.values(reserves)) {
+      const strainValues = Object.values(reserve?.strain ?? {});
+      const minimumFromStrain = strainValues.reduce(
+        (total, isActive) => total + (isActive ? 1 : 0),
+        0
+      );
+      reserve.min = minimumFromStrain;
+    }
   }
 }
