00f834d3cd26d9c3be03ae4bbb20b620d624fef0
GreybeardGM <webmaster@sector-001.de>
2025-11-15 01:19:13 +0100

    Update triskel.js

--- module/triskel.js
+++ module/triskel.js
@@ -1,8 +1,6 @@
 import { TriskelActor } from "./actor/triskel-actor.js";
 import { PlayerCharacter } from "./actor/player-character.js";
 import { PlayerCharacterSheet } from "./actor/sheets/player-character-sheet.js";
-import { TriskelActorDataModel } from "./actor/data/triskel-actor-data.js";
-import { PlayerCharacterDataModel } from "./actor/data/player-character-data.js";
 
 Hooks.once("init", function() {
   console.log("Triskel | Initializing Triskel system");
