297064afe1a8afc14a79e3663d7a5784db0eb82d
GreybeardGM <webmaster@sector-001.de>
2025-11-16 17:55:38 +0100

    Update triskel.js

--- module/triskel.js
+++ module/triskel.js
@@ -5,14 +5,18 @@ import { PlayerCharacterSheet } from "./actor/sheets/player-character-sheet.js";
 Hooks.once("init", function() {
   console.log("Triskel | Initializing Triskel system");
 
+  // Klassen mit Typen verknüpfen
   CONFIG.Actor.documentClass = TriskelActor;
+  CONFIG.Actor.typeClasses.character = PlayerCharacter;
 
+  // Sheets registrieren
   Actors.registerSheet("triskel", PlayerCharacterSheet, {
     makeDefault: true,
     types: ["character"],
     label: "Player Character"
   });
 
+  // L<abens für Typen
   CONFIG.Actor.typeLabels = {
     ...CONFIG.Actor.typeLabels,
     character: "Player Character"
